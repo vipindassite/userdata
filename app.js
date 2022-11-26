bb3951474d190947d1083fd06ecbc9f54fd6196a
vipindassite <118904542+vipindassite@users.noreply.github.com>
2022-11-26 01:00:49 +0000

    some change

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
 
 // database
 const user = require("./databases/userDatabase")()
